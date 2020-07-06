ec39b568b7554f72371287f8c427b975665768c0
Brent Longborough <brent@longborough.org>
2020-07-06 15:16:36 +0100

    Prettify; use Haskell 98-compatible polymorphism

---
+++ Ntutils.hs
@@ -0,0 +1,108 @@
+module Ntutils (
+       iSqrt, iSqrem, isPrTo, isPrime, firstFalse, firstTrue, primeFactors
+       ) where
+
+-- ===========================================================
+-- Two useful lists
+-- dlist:   List of increments between possible primes
+-- plist:   Running total = list of possible primes (all prime to 2,3,5,7)
+-- x is prime => x is a member of plist
+-- -----------------------------------------------------------
+dlist = 2 : 1 : 2 : 2 : 4 :
+ cycle [2,4,2,4,6,2,6,4,2,4,6,6,2,6,4,2,6,4,6,8,4,2,4,2,4,8,6,4,6,2,4,6,2,6,6,4,2,4,6,2,6,4,2,4,2,10,2,10]
+plist = scanl1 (+) dlist
+
+-- ===========================================================
+-- Tests if a number is prime to a single divisor
+-- or to  a list of divisors
+-- -----------------------------------------------------------
+class PrimeTo a where
+    isPrTo :: Integer -> a -> Bool
+instance PrimeTo Integer where
+    isPrTo n d = 0 /= (rem n d)
+instance PrimeToList a => PrimeTo [a] where
+    isPrTo = isPrToList
+
+class PrimeToList a where
+    isPrToList :: Integer -> [a] -> Bool
+instance PrimeToList Integer where
+    isPrToList n = all $ isPrTo n
+
+-- ===========================================================
+-- Shifts a 1 bit up until greater than a value n
+-- Result > n > Result / 4
+-- -----------------------------------------------------------
+bitmatch ::  Integer -> Integer -> Integer
+bitmatch n b
+    | n < b = b
+    | otherwise = bitmatch n (b * 4)
+
+-- ===========================================================
+-- Top-level square root function, uses srqhelper as a helper
+-- iSqrem: Returns (root, remainder)
+-- iSqrt:  Returns root
+-- For iSqrt x,
+--      x == root*root + remainder
+--      x < (root+1) * (root+1)
+-- -----------------------------------------------------------
+iSqrem :: Integer -> (Integer,Integer)
+iSqrem x = srqhelper x 0 (bitmatch x 1)
+iSqrt ::  Integer -> Integer
+iSqrt = fst . iSqrem
+
+-- ===========================================================
+-- Iterative-Recursive square root finder
+-- remainder:   Initially subject, diminishes with progress
+-- root:        Initially zero, develops with progress
+-- onebit:      A single bit, starts at high end, shifts down with progress
+-- -----------------------------------------------------------
+srqhelper :: Integer -> Integer -> Integer -> (Integer,Integer)
+srqhelper remainder root onebit
+    | onebit < 1        = (root,remainder)
+    | remainder >= 0    = srqhelper newrem (div newroot 2) (div onebit 4)
+    | otherwise = error "Attempting to take square root of negative Integer"
+        where
+            try = root + onebit
+            newrem = if remainder >= try then (remainder - try) else remainder
+            newroot = if remainder >= try then (try + onebit) else root
+
+-- ===========================================================
+-- Tests if a number is prime
+-- -----------------------------------------------------------
+isPrime :: Integer -> Bool
+isPrime x = p where
+    p =  firstFalse $ map (isPrTo x) (takeWhile (<= (iSqrt x)) plist)
+
+-- ===========================================================
+-- Searches a finite list for the first False value
+-- -----------------------------------------------------------
+firstFalse :: [Bool] -> Bool
+firstFalse [] = True
+firstFalse (x:xs) = x && (firstFalse xs)
+
+-- ===========================================================
+-- Searches a finite list for the first True value
+-- -----------------------------------------------------------
+firstTrue :: [Bool] -> Bool
+firstTrue [] = False
+firstTrue (x:xs) = x || (firstTrue xs)
+
+-- ===========================================================
+-- Factorization
+-- Given an integer, returns a list of its prime factors
+-- number:  Number to be factored; ground down as factoring proceeds
+-- result:  Factor list; builds up as factoring proceeds
+-- divlist: List of possible prime factors, worn away during operation
+-- For efficiency, intermediate factor list is built in reverse order
+-- -----------------------------------------------------------
+primeFactors :: Integer -> [Integer]
+primeFactors number = reverse $ factorhelper number [] plist
+
+factorhelper :: Integer -> [Integer] -> [Integer] -> [Integer]
+factorhelper number result divlist@(p:ps)
+    | number < 2        = result
+    | isPrime number    = number : result
+    | isPrTo number p   = factorhelper number result ps
+    | otherwise         = factorhelper (div number p) (p : result) divlist
+
+-- ===========================================================
--- primes.hs
+++ primes.hs
@@ -3,16 +3,16 @@ import Isqrt
 printem ::  Show a => [a] -> IO ()
 printem [] = return ()
 printem (x:xs) = do
-    print x 
+    print x
     printem xs
 
 -- ===========================================================
 -- Two useful lists
 -- dlist:   List of increments between possible primes
--- plist:   Running total = list of possible primes (all prime to 2,3,5,7) 
--- x is prime => x is a member of plist 
+-- plist:   Running total = list of possible primes (all prime to 2,3,5,7)
+-- x is prime => x is a member of plist
 -- -----------------------------------------------------------
-dlist = 2 : 1 : 2 : 2 : 4 : 
+dlist = 2 : 1 : 2 : 2 : 4 :
  cycle [2,4,2,4,6,2,6,4,2,4,6,6,2,6,4,2,6,4,6,8,4,2,4,2,4,8,6,4,6,2,4,6,2,6,6,4,2,4,6,2,6,4,2,4,2,10,2,10]
 plist = scanl1 (+) dlist
 
@@ -22,17 +22,25 @@ plist = scanl1 (+) dlist
 -- -----------------------------------------------------------
 class PrimeTo a where
     ispt :: Integer -> a -> Bool
+
 instance PrimeTo Integer where
     ispt n d = 0 /= (rem n d)
-instance PrimeTo a => PrimeTo [a] where
-    ispt n = all (ispt n)
+
+instance PrimeToList a => PrimeTo [a] where
+    ispt = isptList
+
+class PrimeToList a where
+    isptList :: Integer -> [a] -> Bool
+
+instance PrimeToList Integer where
+    isptList n = all $ ispt n
 
 -- ===========================================================
 -- Tests if a number is prime
 -- -----------------------------------------------------------
 isprime :: Integer -> Bool
 isprime x = p where
-    p = firstfalse $ map (ispt x) (takeWhile (<= (isqrt x)) plist) 
+    p = firstfalse $ map (ispt x) (takeWhile (<= (isqrt x)) plist)
 
 -- ===========================================================
 -- Searches a finite list for the first False value
@@ -49,7 +57,7 @@ firsttrue [] = False
 firsttrue (x:xs) = x || (firsttrue xs)
 
 -- ===========================================================
--- Extracts everything up to a given number 
+-- Extracts everything up to a given number
 -- from an infinite, ordered list of integers
 -- -----------------------------------------------------------
 upto :: Integer -> [Integer] -> [Integer] -> [Integer]
@@ -57,11 +65,11 @@ upto _ _ [] = []
 upto n r (x:xs)
     | x > n = r
     | otherwise = upto n (r ++ [x]) xs
-    
+
 factor :: Integer -> [Integer]
 factor n = tail $ reverse $ getfactors n [1] plist
 
-getfactors :: Integer -> [Integer] -> [Integer] -> [Integer]    
+getfactors :: Integer -> [Integer] -> [Integer] -> [Integer]
 getfactors n r all@(p:ps)
     | n < 2 = r
     | isprime n = n : r
@@ -69,7 +77,7 @@ getfactors n r all@(p:ps)
     | otherwise =  getfactors (div n p) (p : r) all
 
 -- ===========================================================
--- Experimental stuff 
+-- Experimental stuff
 -- -----------------------------------------------------------
 l = filter isprime plist
 z = zip (tail plist) plist
@@ -79,11 +87,10 @@ z = zip (tail plist) plist
     -- c = filter (\x -> (ispt x n )) [2..]
     -- d = (upto (last n) [] $ 0:plist)  ++ c
     -- l = map (\x -> (fst x) - (snd x)) $ zip (tail d) d
-    
-    
+
+
 main = do
-    printem $ filter isprime  [2222222.. 3333333] 
+    printem $ filter isprime  [2222222.. 3333333]
     -- printem $ zip [1..100] ( map factor [1..100] )
     -- printem $ take 400 $ zip (delve [2,3,5,7,11]) (scanl1 (+) $ delve [2,3,5,7,11])
     -- print $ factor $ 2147483647
-    
